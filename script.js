147f93e0684b3189b52319bcd72b7c74045fe60c
AliDaniel1 <alidanielh123@gmail.com>
2022-05-15 22:29:37 -0430

    Commit#1

---
+++ script.js
@@ -0,0 +1,14 @@
+var score = document.getElementById("score");
+var x = 0;
+
+alert("Obten 80 puntos para ganar");
+let name = prompt("Ingresa tu nombre");
+
+function Puntos(a){
+    x = x + a
+    score.innerHTML = x;
+    if(x > 80){
+        x = x - 80
+        score.innerHTML = "<h1>!!Felicitaciones " + name + " Has Ganado!!"
+    };
+};
